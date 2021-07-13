39b7e18b399f0da01cc998a5f890a1e5a1442876
James Johnson <james.johnson@tiny.cloud>
2021-07-13 17:22:23 +1000

    Added inline comments to the routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -158,27 +158,32 @@ const hasLock = async (req, res, next) => {
 router.use('/', swaggerUi.serve);
 router.get('/', swaggerUi.setup(swaggerDocument));
 
-// generate a jwt token for a user
+// generate a JSON web token for a user to represent their login session
 router.post('/jwt', async function (req, res, next) {
+  // get the username and password from the body of the request
   const username = req.body.username;
   const password = req.body.password;
+  // validate inputs
   if (typeof username !== 'string' && /^[a-zA-Z0-9~_.-]+$/.test(username)) {
     return res.status(400).json({ success: false, message: 'The username must be a string matching the pattern /^[a-zA-Z0-9~_.-]+$/ .' });
   }
   if (typeof password !== 'string' || passport.length === 0) {
     return res.status(400).json({ success: false, message: 'The password property must be a non-empty string.' });
   }
+  // lookup user record in database
   rows = await req.db.select('hash').from('users').where('username', username);
   if (rows.length === 0) {
     res.status(401).json({ success: false, message: 'Incorrect username or password.' });
     return;
   }
+  // check that the password was correct
   const { hash } = rows[0];
   const valid = await bcrypt.compare(password, hash);
   if (!valid) {
     res.status(401).json({ success: false, message: 'Incorrect username or password.' });
     return;
   }
+  // create a new JSON web token linked to the user that expires in 1 day
   const token = jwt.sign({}, process.env.PRIVATE_KEY, { subject: username, expiresIn: '1d', algorithm: 'RS256' });
   res.json({ success: true, token });
 });
@@ -191,9 +196,11 @@ router.get('/users', async function (req, res, next) {
 
 // create a new user
 router.post('/users', async function (req, res, next) {
+  // get the username, password and the full name from the body of the request
   const username = req.body.username;
   const password = req.body.password;
   const fullName = req.body.fullName;
+  // validate the inputs
   if (typeof username !== 'string' && /^[a-zA-Z0-9~_.-]+$/.test(username)) {
     return res.status(400).json({ success: false, message: 'The username must be a string matching the pattern /^[a-zA-Z0-9~_.-]+$/ .' });
   }
@@ -205,12 +212,17 @@ router.post('/users', async function (req, res, next) {
   }
   // increase the difficulty of generating the hash to make reverse engineering passwords very computationally intensive
   const rounds = 12;
-  // note that this generates a salt, uses it to hash the password and then returns the "<salt>.<hash>" combination
+  // note that this generates a salt, uses it to hash the password and then returns an encoded salt + hash combination
   const hash = await bcrypt.hash(password, rounds);
   try {
+    // the users table has a uniqueness constraint on the username so if this
+    // is a duplicate it will throw an error
     await req.db.insert({ username, hash, fullName }).into('users');
     res.status(201).json({ success: true });
   } catch (e) {
+    // for the sake of simplicity we're assuming that all failures are due to 
+    // the user already existing though there are other failure options 
+    // like for example a missing database
     console.log(e);
     res.status(409).json({ success: false, message: 'A user already exists with that username.' });
   }
@@ -218,19 +230,23 @@ router.post('/users', async function (req, res, next) {
 
 // get user details
 router.get('/users/:username', isAuthenticated, async function (req, res, next) {
+  // lookup the user in the database and get their details (currently just their full name)
   const rows = await (
     req.db.select('fullName')
       .from('users')
       .where('username', '=', req.params.username)
   );
+  // check the user exists
   if (rows.length === 0) {
     return res.status(404).json({ success: false, message: 'No user was found with that username.' });
   }
+  // return the user details
   return res.json({ success: true, fullName: rows[0].fullName });
 });
 
 // get all documents
 router.get('/documents', isAuthenticated, async function (req, res, next) {
+  // select all document UUIDs where the authenticated user has some permissions
   const documents = await (
     req.db.select('documents.uuid')
       .from('documents')
@@ -239,40 +255,56 @@ router.get('/documents', isAuthenticated, async function (req, res, next) {
       .andWhere('collaborators.permissions', '>=', 1)
       .pluck('uuid')
   );
+  // return the documents UUIDs
   res.json({ success: true, documents });
 });
 
 // create a new document
 router.post('/documents', isAuthenticated, async function (req, res, next) {
+  // get the new document title from the request body
   const title = req.body.title;
+  // validate that the title is not empty.
   if (!title) {
-    res.status(400).json({ success: false, messsage: 'The title is required.' });
+    res.status(400).json({ success: false, message: 'The title is required.' });
   }
   const username = req.user;
+  // generate a new randomly generated universally unique identifier
+  // https://en.wikipedia.org/wiki/Universally_unique_identifier#Version_4_(random)
   const uuid = uuidV4();
+  // create the new document
   await req.db.insert({ uuid, title }).into('documents');
+  // give the current user full access to the document
   await req.db.insert({ document: uuid, user: username, permissions: (PERMISSION_MANAGE | PERMISSION_WRITE | PERMISSION_READ) }).into('collaborators');
+  // return the new document's UUID
   res.json({ success: true, uuid });
 });
 
 // acquire or release a document lock
 router.put('/documents/:documentUuid/lock', isAuthenticated, hasWritePermission, async function (req, res, next) {
+  // get the authenticated user and the document UUID and assert they exist (which they must due to the previous middleware)
   const user = req.user;
   const uuid = req.params.documentUuid;
   if (!user) throw new Error('User must be authenticated');
   if (uuid === undefined) throw new Error('Path must contain parameter documentUuid');
+  // get the release flag from the request body or a default
   const release = req.body.release ?? false;
+  // validate the release flag
   if (typeof release !== 'boolean') {
     res.status(400).json({ success: false, message: 'The release option must be true or false if provided.' });
     return;
   }
+  // now check if the document lock must be acquired or released
   if (release) {
     // release the lock if the user holds it
     await req.db('documents').
       where({ uuid: uuid, lockUser: user }).
       update({ 'lockUser': null, 'lockTime': null });
+    // we are always successful in giving up the lock, 
+    // however that does not mean that we owned it.
     res.json({ success: true, release });
   } else {
+    // the user wishes to acquire the lock
+    // in case there is an existing lock calculate how new it must be to be valid
     const now = DateTime.now().toISO();
     const oldestValidLock = DateTime.now().minus({ seconds: 60 }).toISO();
     // attempt to acquire the lock
@@ -292,34 +324,47 @@ router.put('/documents/:documentUuid/lock', isAuthenticated, hasWritePermission,
 
 // get document title
 router.get('/documents/:documentUuid/title', isAuthenticated, hasReadPermission, async function (req, res, next) {
+  // get the document title from the database
   const [{ title }] = await req.db.select('title').from('documents').where('uuid', req.params.documentUuid);
+  // return the document title
   res.json({ success: true, title });
 });
 
 // get document content
 router.get('/documents/:documentUuid/content', isAuthenticated, hasReadPermission, async function (req, res, next) {
+  // get the document content from the database
   const [{ content }] = await req.db.select('content').from('documents').where('uuid', req.params.documentUuid);
+  // return the document content
   res.json({ success: true, content });
 });
 
 // store document content
 router.put('/documents/:documentUuid/content', isAuthenticated, hasWritePermission, hasLock, async function (req, res, next) {
+  // get the document content from the body of the request
   const content = req.body.content;
+  // validate the content
   if (typeof content !== 'string') {
     res.status(400).json({ success: false, message: 'The content is required.' });
     return;
   }
+  // update the document content
   await req.db('documents').where('uuid', req.params.documentUuid).update({ content });
+  // return success
   res.status(200).json({ success: true });
 });
 
 // get list of users with access
 router.get('/documents/:documentUuid/collaborators', isAuthenticated, hasReadPermission, async function (req, res, next) {
-  const rows = await req.db.select('user', 'permissions').from('collaborators').where('document', req.params.documentUuid).orderBy([{ column: 'permissions', order: 'desc' }, { column: 'user', order: 'asc' }]);
+  // get all usernames and permissions on the document sorted by most permissions to least
+  const rows = await req.db.select('user', 'permissions').from('collaborators').
+    where('document', req.params.documentUuid).
+    orderBy([{ column: 'permissions', order: 'desc' }, { column: 'user', order: 'asc' }]);
+  // convert the user permissions into roles
   const collaborators = rows.map(({ user, permissions }) => ({
     username: user,
     role: permissionsToRole(permissions)
   }));
+  // return the collaborators
   res.json({ success: true, collaborators });
 });
 
@@ -335,25 +380,29 @@ router.put('/documents/:documentUuid/collaborators/:username', isAuthenticated, 
   if (!Object.keys(ROLES).includes(role)) {
     return res.status(400).json({ success: false, message: 'The role must be one of: "' + ROLE_ORD.join('", "') + '".' });
   }
+  // check that the specified user actually exists
   const userExists = await req.db.select('*').from('users').where('username', user);
   if (userExists.length !== 1) {
     return res.status(400).json({ success: false, message: 'The username does not refer to a user that exists.' });
   }
+  // get the numerical value of the role
   const permissions = ROLES[role];
-  // check if a record exists
+  // check if a collaborator record exists
   const existing = await req.db.select('*').from('collaborators').where({ 'document': req.params.documentUuid, 'user': user });
+  // update, delete or create the collaborator record
   if (existing.length > 0) {
     if (permissions !== 0) {
-      // update existing record
+      // update the existing collaborator record
       await req.db('collaborators').where({ document, user }).update({ permissions });
     } else {
-      // delete existing record
+      // delete the existing collaborator record
       await req.db('collaborators').where({ document, user }).del();
     }
   } else if (permissions !== 0) {
-    // insert new record
+    // insert new collaborator record
     await req.db.insert({ document, user, permissions }).into('collaborators');
   }
+  // return success
   return res.json({ success: true });
 });
 
