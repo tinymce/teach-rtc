90f89b2b720ea4e2b8a78544ade876ee9b5679c6
James Johnson <james.johnson@tiny.cloud>
2021-07-15 13:24:38 +1000

    Added comments to RTC configuration

--- client/src/pages/DocumentEdit.js
+++ client/src/pages/DocumentEdit.js
@@ -6,6 +6,13 @@ import { useState } from 'react';
 import ConnectedClient from '../components/ConnectedClient';
 import {isMobile} from 'react-device-detect';
 
+/**
+ * Save the editor content.
+ * @param {object} inputs the inputs.
+ * @param {string} inputs.documentId the document ID.
+ * @param {number} inputs.version the document version.
+ * @param {() => string} inputs.getContent a function to request the serialized content. 
+ */
 const saveSnapshot = ({ documentId, version, getContent }) => {
   saveContent({ documentId, version, content: getContent() });
 };
@@ -51,14 +58,49 @@ export default function DocumentEdit({ token }) {
         init={{
           ...config,
           plugins: 'rtc ' + config.plugins,
+          // RTC uses a unique ID to identify documents which is provided
+          // by your application.
           rtc_document_id: documentId,
+          // To ensure privacy of your content the RTC plugin encrypts all
+          // messages that need to be sent to the RTC server.
+          // The key to encrypt the messages is provided by your application on
+          // request by the RTC plugin.
+          // The server never has access to the encryption key so it can't view
+          // the contents of the document.
           rtc_encryption_provider: getSecretKey,
+          // The future plan for RTC is that users will be authorized to view
+          // or edit documents with JSON web tokens that contain claims
+          // identifying the document and the role that the user has.
+          // This example is written as if this is currently possible...
+          // Currently the only requirement is that the JWT is signed by a 
+          // key pair that is registered with the TinyMCE cloud account to 
+          // prove it has access.
           rtc_token_provider: () => getJwt({ documentId }),
+          // The first time that RTC loads on a document when it hasn't seen
+          // the document ID before it will get the starting content from your
+          // application.
+          // All later times the encrypted snapshots and messages will be 
+          // retrieved from the RTC server and replayed on the client to
+          // recreate the document content.
           rtc_initial_content_provider: getContent,
+          // The RTC plugin periodically calls this function to allow
+          // integrators to save the document. The version number
+          // provided allows the integrator to tell old and new snapshots apart.
           rtc_snapshot: saveSnapshot,
+          // The RTC plugin calls this function when it encounters a new user.
+          // This provides the full name of the user by default but any information
+          // that can be encoded as JSON can be passed.
           rtc_user_details_provider: getUserDetails,
+          // The RTC plugin calls this method when a client connects.
+          // This includes the details from the rtc_user_details_provider and
+          // the extra data provided to rtc_client_info.
           rtc_client_connected: clientConnected,
+          // The RTC plugin calls this method when a client disconnects.
+          // This includes the details from the rtc_user_details_provider and
+          // the extra data provided to rtc_client_info.
           rtc_client_disconnected: clientDisconnected,
+          // The RTC plugin transmits this data to all other clients.
+          // The only restriction is that this must be encodable as JSON.
           rtc_client_info: { isMobile },
         }}
       />
