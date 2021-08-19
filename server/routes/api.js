79f072bca7eb3220d51b16fdd01636fde382d729
James Johnson <james.johnson@tiny.cloud>
2021-08-19 16:30:32 +1000

    Use express-promise-router to handle server errors

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require("express-promise-router")();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const swaggerUi = require('swagger-ui-express');
